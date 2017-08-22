b8f4605f07a7b2422eecea2dd2047954c6835bf8
Eli Qiao <liyong.qiao@intel.com>
2017-08-22 14:12:21 +0800

    Add go:generate comment
    use go generate ./... to generate mock file which has go:generate
    comment

--- doer/doer.go
+++ doer/doer.go
@@ -1,5 +1,7 @@
 package doer
 
+//go:generate mockgen -destination=../mocks/mock_doer.go -package=mocks github.com/taget/testMock/doer Doer
+
 type Doer interface {
 	DoSomething(int, string) error
 }
